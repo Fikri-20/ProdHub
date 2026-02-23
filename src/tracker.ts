33ede0df8f7ed6b4649785f847f25e434312e1e1
Fikri <dev.fikrii@gmail.com>
2026-02-23 08:09:55 +0200

    feat: write a module that polls the active window every 5 seconds

--- src/tracker.ts
+++ src/tracker.ts
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import { activeWindow } from "get-windows";
+
+let lastApp = "";
+setInterval(async () => {
+  const win = await activeWindow();
+
+  if (!win) {
+    console.log("No active window found.");
+    return;
+  }
+
+  if (lastApp !== win.owner.name) {
+    console.log("Window title:", win.title);
+    console.log("Application:", win.owner.name);
+    console.log("Application path:", win.owner.path);
+    console.log("Application PID:", win.owner.processId);
+    lastApp = win.owner.name;
+  }
+}, 5000);
